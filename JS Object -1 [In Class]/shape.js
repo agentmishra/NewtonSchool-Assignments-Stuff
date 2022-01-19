97fa8cb2f190631b02eaa424e8542b8f530b6e07
Amit Kumar Mishra <32227450+agentmishra@users.noreply.github.com>
2022-01-19 23:51:24 +0530

    Create shape.js

---
+++ JS Object -1 [In Class]/shape.js
@@ -0,0 +1,14 @@
+// declare class
+class Shape {
+     constructor() {
+          this.color = "green";
+     }
+     drawShape() {
+
+     }
+     calculateArea() {
+
+     }
+}
+// export class using module.exports
+module.exports = Shape;
