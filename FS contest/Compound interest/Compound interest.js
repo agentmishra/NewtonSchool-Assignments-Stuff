bfbac0541495cf2b3f70ec5cd850d045d4bfb328
Amit Kumar Mishra <32227450+agentmishra@users.noreply.github.com>
2022-01-19 23:28:08 +0530

    Create Compound interest.js

---
+++ FS contest/Compound interest/Compound interest.js
@@ -0,0 +1,10 @@
+// You need to return Compound Interest
+// P: principal value
+// T: rate of interest
+// T: time in years 
+function calculateCI(P, R, T)
+{
+       // Your code here
+       const amount = P * Math.pow((1 + R / 100), T);
+       return (amount-P).toFixed(2);
+}
