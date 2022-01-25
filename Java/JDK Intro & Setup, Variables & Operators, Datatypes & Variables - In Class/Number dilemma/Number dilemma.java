c1ed82dff16f1d0cd8b5492a723344bdbb2c64c4
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-01-25 22:56:37 +0530

    Create Number dilemma.java

---
+++ Java/JDK Intro & Setup, Variables & Operators, Datatypes & Variables - In Class/Number dilemma/Number dilemma.java
@@ -0,0 +1,12 @@
+static int StringToInt(String S){
+//Enter your code here
+int num = Integer.parseInt(S);
+return num;
+
+}
+
+static String IntToString(int N){
+//Enter your code here
+String s=String.valueOf(N);
+return s;
+}
