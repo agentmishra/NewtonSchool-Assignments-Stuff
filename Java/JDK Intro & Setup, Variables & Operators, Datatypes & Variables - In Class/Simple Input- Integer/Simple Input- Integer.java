c8eb3942e8a2de57411ca17eea03ca02b74b8775
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-01-25 22:53:21 +0530

    Create Simple Input- Integer.java

---
+++ Java/JDK Intro & Setup, Variables & Operators, Datatypes & Variables - In Class/Simple Input- Integer/Simple Input- Integer.java
@@ -0,0 +1,14 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+                      // Your code here
+        int a;
+        Scanner obj = new Scanner(System.in);
+        a = obj.nextInt(); 
+        System.out.println(a);
+	}
+}
