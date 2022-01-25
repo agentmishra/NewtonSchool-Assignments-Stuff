59d8d82f10b389cc7e1e587de3916f5a7f36e821
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-01-25 22:51:26 +0530

    Create Simple Input- String.java

---
+++ Java/JDK Intro & Setup, Variables & Operators, Datatypes & Variables - In Class/Simple Input- String/Simple Input- String.java
@@ -0,0 +1,14 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+                      // Your code here
+
+	Scanner sc= new Scanner(System.in); 
+	//reads string   
+	System.out.print(S);  
+	}
+}
