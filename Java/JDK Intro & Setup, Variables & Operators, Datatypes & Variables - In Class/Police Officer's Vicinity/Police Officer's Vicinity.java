a8f39d22c549732fe6c29d17a6263055026dd45a
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-01-25 23:56:01 +0530

    Update Police Officer's Vicinity.java

--- Java/JDK Intro & Setup, Variables & Operators, Datatypes & Variables - In Class/Police Officer's Vicinity/Police Officer's Vicinity.java
+++ Java/JDK Intro & Setup, Variables & Operators, Datatypes & Variables - In Class/Police Officer's Vicinity/Police Officer's Vicinity.java
@@ -1,1 +1,38 @@
-did not do yet
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+        // Your code here
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		int e = scan.nextInt();
+		int x = scan.nextInt();
+		int y = scan.nextInt();
+		int k = scan.nextInt();
+		int u=n,d=1,l=1,r=e;
+		for(int i=0;i<k;i++)
+		{
+			int a,b;
+			a = scan.nextInt();
+			b = scan.nextInt();
+			if(a==x)
+			{
+				if(b<y)
+					d=b+1;
+				else
+					u=b-1;
+			}
+			else if(b==y)
+			{
+				if(a<x)
+					l=a+1;
+				else
+					r=a-1;
+			}
+		}
+		System.out.println(r-l+u-d);
+	}
+}
