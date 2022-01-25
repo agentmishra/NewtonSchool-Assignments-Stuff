6ac1510bba6851ad61668ca49b1886e200fc2897
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-01-25 22:54:54 +0530

    Create Triangle.java

---
+++ Java/JDK Intro & Setup, Variables & Operators, Datatypes & Variables - In Class/Triangle/Triangle.java
@@ -0,0 +1,15 @@
+static void Triangle(){
+//Enter your code here
+
+for (int i= 4; i>=0 ; i--)  
+{  
+
+for (int j=0; j<=i; j++)  
+{  
+/
+System.out.print("*");  
+}  
+
+System.out.println();  
+}  
+}
