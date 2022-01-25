b6af43a771508435a1d863e2db675184d0cd9d45
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-01-25 23:09:35 +0530

    Create Simple Sum.java

---
+++ Java/JDK Intro & Setup, Variables & Operators, Input, Output and Exceptions, Con ... - In Class/Simple Sum/Simple Sum.java
@@ -0,0 +1,29 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+                      // Your code here
+					  Scanner input = new Scanner(System.in);
+
+int A;
+
+int B;
+
+int C;
+
+int sum;
+
+A = input.nextInt();
+
+B = input.nextInt();
+
+C = input.nextInt();
+
+sum = A+B+C;
+
+System.out.println(sum);
+	}
+}
