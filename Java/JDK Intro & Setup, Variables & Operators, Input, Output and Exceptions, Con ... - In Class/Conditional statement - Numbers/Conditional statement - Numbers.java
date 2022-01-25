d59aff6407bccd759f9af707c58a1ae26bfac147
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-01-25 23:05:07 +0530

    Create Conditional statement - Numbers.java

---
+++ Java/JDK Intro & Setup, Variables & Operators, Input, Output and Exceptions, Con ... - In Class/Conditional statement - Numbers/Conditional statement - Numbers.java
@@ -0,0 +1,22 @@
+static String conditional(int n){
+    String myOutput="";
+        if(n==1){
+            myOutput="one";    
+        }
+        else if(n==2){
+            myOutput="two"; 
+        }
+        else if(n==3){
+            myOutput="three"; 
+        }
+        else if(n==4){
+            myOutput="four"; 
+        }
+        else if(n==5){
+            myOutput="five"; 
+        }
+        else{
+            myOutput="Greater than 5"; 
+        }
+    return myOutput;
+}
