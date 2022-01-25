88ddac9ae177230ef2f570de1a7ec667cce62578
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-01-25 22:36:21 +0530

    Create Vowel or Consonant.java

---
+++ Java/JDK Intro & Setup, Variables & Operators, Datatypes & Variables - In Class/Vowel or Consonant/Vowel or Consonant.java
@@ -0,0 +1,9 @@
+static void VowelorConsonant(char C){
+//Enter your code here
+
+        if (C == 'a' || C == 'e' || C == 'i' || C == 'o'
+            || C == 'u')
+            System.out.println("V");
+        else
+            System.out.println("C");
+    }
