c9b17a232bbe4ecb4e3e1f6df6b1292fd5fe35cb
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-01-25 22:59:04 +0530

    Create Simple Clock Angle.java

---
+++ Java/JDK Intro & Setup, Variables & Operators, Datatypes & Variables - In Class/Simple Clock Angle/Simple Clock Angle.java
@@ -0,0 +1,17 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+                      // Your code here
+
+       
+    Scanner obj = new Scanner(System.in);
+    int N = obj.nextInt(); 
+    
+    System.out.println(N*6);
+
+	}
+}
