81ddda97c923cc6ea2d391c5f99bab359ac41d80
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-01-25 22:34:12 +0530

    Create Logical operators.java

---
+++ Java/JDK Intro & Setup, Variables & Operators, Datatypes & Variables - In Class/Logical operators/Logical operators.java
@@ -0,0 +1,30 @@
+static boolean Logical_AND(int a, int b){
+//Enter your code here
+if ((a <= 10) && (b >= 10)) {
+        return true;
+        }
+    else{
+        return false;
+    }
+
+}
+static boolean Logical_OR(int a, int b){
+//Enter your code here
+if ((a%2==0) || (b%2==0)) {
+        return true;
+        }
+    else{
+        return false;
+    }
+
+}
+static boolean Logical_NOT(int a, int b){
+//Enter your code here
+if (a!=b) {
+        return true;
+        }
+    else{
+        return false;
+    }
+
+}
