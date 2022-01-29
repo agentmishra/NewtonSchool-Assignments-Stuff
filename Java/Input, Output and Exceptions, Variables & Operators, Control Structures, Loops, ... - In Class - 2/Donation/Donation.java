b13e168cfbd2b9d5f50abdff1393427ad40d0416
Amit Kumar Mishra <mishra.amitkr@gmail.com>
2022-01-29 16:50:18 +0530

    Update Donation.java

--- Java/Input, Output and Exceptions, Variables & Operators, Control Structures, Loops, ... - In Class - 2/Donation/Donation.java
+++ Java/Input, Output and Exceptions, Variables & Operators, Control Structures, Loops, ... - In Class - 2/Donation/Donation.java
@@ -1,1 +1,37 @@
-did not complete yet
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int[] arr = new int[N]; 
+        int prev = sc.nextInt() ;
+        arr[0]=prev;
+        long sum = prev ;
+        System.out.print("0 ");
+        for(int i = 1 ; i < N ; i++){
+            arr[i] = sc.nextInt();
+            prev=arr[i-1];
+            if(arr[i] < prev)
+            {
+                System.out.print( (prev - arr[i]) + " ");
+                
+                sum = sum + arr[i]+(prev - arr[i]);
+                arr[i] = prev ;
+                
+            }
+            else
+            {
+                System.out.print("0 ");
+                
+                sum = sum + arr[i];     
+            }      
+        }
+        System.out.println();        
+        System.out.print(sum);
+	}
+}
